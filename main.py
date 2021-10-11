a2005c1c55456e939c42303da072499cf11e1127
Kelvin <yiqing.cheng@emory.edu>
2021-10-11 15:01:14 -0400

    Added sensitivity

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from matplotlib import pyplot as plt
 import sklearn.metrics
 from sklearn.feature_selection import SelectFromModel
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, roc_auc_score, precision_score, recall_score
+from sklearn.metrics import accuracy_score, roc_auc_score, precision_score, recall_score, confusion_matrix
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split, RepeatedStratifiedKFold, cross_val_score
@@ -55,13 +55,14 @@ print(x.columns)
 
 #%%
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
-#%%
 logistic_result = list()
 xgboost_result = list()
 adaboosts_result = list()
 rd_result = list()
 SVC_result = list()
 feature_result = list()
+knn_result = list()
+
 
 logistic_precision = list()
 xgboost_precision = list()
@@ -69,6 +70,7 @@ adaboosts_precision = list()
 rd_precision = list()
 SVC_precision = list()
 feature_precision = list()
+knn_precision = list()
 
 
 logistic_recall = list()
@@ -76,7 +78,23 @@ xgboost_recall = list()
 adaboosts_recall = list()
 rd_recall = list()
 SVC_recall = list()
-feature_recall = list()
+knn_recall = list()
+
+
+logistic_specificity = list()
+xgboost_specificity = list()
+adaboosts_specificity = list()
+rd_specificity = list()
+SVC_specificity = list()
+knn_specificity = list()
+
+logistic_sensitivity = list()
+xgboost_sensitivity = list()
+adaboosts_sensitivity = list()
+rd_sensitivity = list()
+SVC_sensitivity = list()
+knn_sensitivity = list()
+
 
 
 #%%
@@ -91,9 +109,14 @@ for i in range(500):
     lrprecision = precision_score(y_test, y_predict_logistic)
     logistic_precision.append(lrprecision)
 
-
     lrrecall = recall_score(y_test, y_predict_logistic)
     logistic_recall.append(lrrecall)
+
+    tn, fp, fn, tp = confusion_matrix(y_test, y_predict_logistic).ravel()
+    specificity = tn / (tn + fp)
+    sensitivity = tp / (tp + fn)
+    logistic_specificity.append(specificity)
+    logistic_sensitivity.append(sensitivity)
 
 
     # xgboost
@@ -107,6 +130,14 @@ for i in range(500):
     xgboost_precision.append(xgprecision)
     xgboost_recall.append(recall_score(y_test, y_predict_xgboost))
 
+    tn, fp, fn, tp = confusion_matrix(y_test, y_predict_xgboost).ravel()
+    specificity = tn / (tn + fp)
+    sensitivity = tp / (tp + fn)
+    xgboost_specificity.append(specificity)
+    xgboost_sensitivity.append(sensitivity)
+
+
+
     # adaboost
     adaboost = AdaBoostClassifier(n_estimators=500, learning_rate=0.01, random_state=0)
     adaboost_fitted = adaboost.fit(X_train, y_train)
@@ -119,6 +150,14 @@ for i in range(500):
 
     adaboosts_recall.append(recall_score(y_test, y_pred_adaboost))
 
+    tn, fp, fn, tp = confusion_matrix(y_test, y_pred_adaboost).ravel()
+    specificity = tn / (tn + fp)
+    sensitivity = tp / (tp + fn)
+    adaboosts_specificity.append(specificity)
+    adaboosts_sensitivity.append(sensitivity)
+
+
+
     # random forest
     clf=RandomForestClassifier(n_estimators=100)
     clf.fit(X_train,y_train)
@@ -130,8 +169,16 @@ for i in range(500):
 
     rdprecision = precision_score(y_test, y_pred)
     rd_precision.append(rdprecision)
-
     rd_recall.append(recall_score(y_test, y_pred))
+
+
+    tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
+    specificity = tn / (tn + fp)
+    sensitivity = tp / (tp + fn)
+    rd_specificity.append(specificity)
+    rd_sensitivity.append(sensitivity)
+
+
 
     # SVC
     SVC_model = svm.SVC(kernel='linear') # Linear Kernel
@@ -142,8 +189,31 @@ for i in range(500):
 
     svcprecision = precision_score(y_test, y_pred)
     SVC_precision.append(svcprecision)
-
     SVC_recall.append(recall_score(y_test, y_pred))
+
+    tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
+    specificity = tn / (tn + fp)
+    sensitivity = tp / (tp + fn)
+    SVC_specificity.append(specificity)
+    SVC_sensitivity.append(sensitivity)
+
+    # SVC
+    from sklearn.neighbors import KNeighborsClassifier
+    classifier = KNeighborsClassifier(n_neighbors=5)
+    classifier.fit(X_train, y_train)
+    y_pred = classifier.predict(X_test)
+    KNN_Acc = accuracy_score(y_test, y_pred)
+    knn_result.append(KNN_Acc)
+
+    knnprecision = precision_score(y_test, y_pred)
+    knn_precision.append(knnprecision)
+    knn_recall.append(recall_score(y_test, y_pred))
+
+    tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
+    specificity = tn / (tn + fp)
+    sensitivity = tp / (tp + fn)
+    knn_specificity.append(specificity)
+    knn_sensitivity.append(sensitivity)
 
 
 #%%
@@ -153,6 +223,7 @@ df_plot = pd.DataFrame({
         'adaboosts': adaboosts_result,
         'RD': rd_result,
         'SVC': SVC_result,
+        'knn': knn_result
     })
 
 plt.title('Model Accuracy for Different Algorithms')
@@ -170,7 +241,9 @@ df_plot_precision = pd.DataFrame({
         'adaboosts': adaboosts_precision,
         'RD': rd_precision,
         'SVC': SVC_precision,
-    })
+        'knn': knn_precision
+
+})
 
 plt.title('Precision for Different Algorithms')
 plt.ylabel('Precision Score')
@@ -184,13 +257,49 @@ df_plot_recall = pd.DataFrame({
         'adaboosts': adaboosts_recall,
         'RD': rd_recall,
         'SVC': SVC_recall,
-    })
+        'knn': knn_recall
+
+})
 
 plt.title('Recall for Different Algorithms')
 plt.ylabel('Recall Score')
 boxplot_recall = df_plot_recall.boxplot()
 boxplot_recall.plot()
 plt.show()
+
+#%%
+df_plot_specificity = pd.DataFrame({
+        'LR': logistic_specificity,
+        'XGboost': xgboost_specificity,
+        'adaboosts': adaboosts_specificity,
+        'RD': rd_specificity,
+        'SVC': SVC_specificity,
+        'knn': knn_specificity
+
+})
+
+plt.title('Specificity for Different Algorithms')
+plt.ylabel('Specificity Score')
+boxplot_specificity = df_plot_recall.boxplot()
+boxplot_specificity.plot()
+plt.show()
+#%%
+df_plot_sensitivity = pd.DataFrame({
+        'LR': logistic_sensitivity,
+        'XGboost': xgboost_sensitivity,
+        'adaboosts': adaboosts_sensitivity,
+        'RD': rd_sensitivity,
+        'SVC': SVC_sensitivity,
+        'knn': knn_sensitivity
+
+})
+
+plt.title('Sensitivity for Different Algorithms')
+plt.ylabel('Sensitivity Score')
+boxplot_sensitivity = df_plot_sensitivity.boxplot()
+boxplot_sensitivity.plot()
+plt.show()
+
 #%%
 plt.subplot(211)
 boxplot_acc = df_plot.boxplot()
@@ -216,6 +325,7 @@ print(statistics.stdev(xgboost_result))
 xgboost = GradientBoostingClassifier(n_estimators=5000, learning_rate=0.001, random_state=0)
 xgboostFitted = xgboost.fit(X_train, y_train)
 y_predict_xgboost = xgboostFitted.predict(X_test)
+
 #%%
 feature_importance = list(xgboostFitted.feature_importances_)
 colnamePlot = list(X_train.columns)
